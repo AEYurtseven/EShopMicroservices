82f97bee461c2cdba9d43bc9e22c532805496533
Ahmet Erdal Yurtseven <erdal.yurtseven.555@gmail.com>
2024-11-25 21:16:21 +0000

    Building Blocks: --Behaviors: Logging Behavior Validation Behavior
    --CQRS
    ICommandHandler
    IQueryHandler
    
    --Exceptions
    CustomExceptionHandler
    BadRequestException
    InternalServerException
    NotFoundException
    
    Catalog.API
    GlobalUsing
    Program

--- eshop-microservices-starter/Catalog.API/Program.cs
+++ eshop-microservices-starter/Catalog.API/Program.cs
@@ -1,6 +1,34 @@
+using BuildingBlocks.Exceptions.Handler;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
